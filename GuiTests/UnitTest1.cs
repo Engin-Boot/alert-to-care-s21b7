aad2159a4363f785bb43b1fed2e0b58f2a823ebc
venkatesh-cs <venkatesh_cs@outlook.com>
2020-10-29 21:00:30 +0530

    Made changes

--- GuiTests/UnitTest1.cs
+++ GuiTests/UnitTest1.cs
@@ -6,19 +6,8 @@ namespace GuiTests
 {
     public class OccupancyTests
     {
-        public OccupancyTests()
-        {
-            _url = "http://localhost:5000/api/";
-        }
-        
-
         [Fact]
-        public void TestIcu()
-        {
-
-        }
-
-        private static string _url;
+        public void 
     }
 }
 
